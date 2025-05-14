e79cf435a53776761a0486f645d896bbff6b030e
Jucélio Brandão <juceneon@gmail.com>
2025-05-13 22:31:28 -0300

    Ajusta nome swagger

--- Quod.API/Program.cs
+++ Quod.API/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AntiFraudAPI v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "QuodChallenge v1");
         c.RoutePrefix = string.Empty;
     });
 }
